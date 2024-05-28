6990bed393773707cc964dcaa73bd822d6217eb8
‘fujisoftzhang’ <2510906118@qq.com>
2024-05-28 11:54:59 +0900

    Type error: Cannot find name 'links'. Did you mean 'Link'? 错误解决

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -9,13 +9,23 @@ import {
   import { usePathname } from 'next/navigation';
   import clsx from 'clsx';
    
-  // ...
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const linksList = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
    
   export default function NavLinks() {
     const pathname = usePathname();
     return (
       <>
-        {links.map((link) => {
+        {linksList.map((link) => {
           const LinkIcon = link.icon;
           return (
             <Link
